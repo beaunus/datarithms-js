d43dd2b65a4c381c5d05c4dc50e3f5efb8ca1635
Beau Dobbin <beau@beaunus.com>
2018-10-24 21:48:45 +0900

    write 3sum and tests

---
+++ algorithms/three-sum.js
@@ -0,0 +1,35 @@
+import { indexOf } from "./binary-search";
+
+/**
+ * @param {Array} array
+ */
+function bruteForce(array) {
+  let count = 0;
+  for (let i = 0; i < array.length; ++i) {
+    for (let j = i + 1; j < array.length; ++j) {
+      for (let k = j + 1; k < array.length; ++k) {
+        if (array[i] + array[j] + array[k] === 0) {
+          ++count;
+        }
+      }
+    }
+  }
+  return count;
+}
+
+/**
+ * @param {Array} array
+ */
+function quadratic(array) {
+  array.sort((a, b) => a - b);
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      const k = indexOf(array, -(array[i] + array[j]));
+      if (k > j) ++count;
+    }
+  }
+  return count;
+}
+
+export { bruteForce, quadratic };
---
+++ algorithms/three-sum.spec.js
@@ -0,0 +1,36 @@
+import { expect } from "chai";
+import _ from "lodash";
+
+import { bruteForce, quadratic } from "./three-sum";
+
+describe("three-sum", () => {
+  describe("bruteForce", () => {
+    it("should be a function", () => {
+      expect(bruteForce).to.be.a("function");
+    });
+    it("should return 1 when the array is super simple", () => {
+      const array = [1, -1, 0];
+      expect(bruteForce(array)).to.equal(1);
+    });
+  });
+  describe("quadratic", () => {
+    it("should return the same answer as bruteForce", () => {
+      const MAX_LENGTH = 100;
+
+      for (let length = 3; length <= MAX_LENGTH; ++length) {
+        const elements = new Set();
+        while (elements.size < length) {
+          let element = Math.floor(Math.random() * length);
+          if (Math.random() > 0.5) {
+            element *= -1;
+          }
+          elements.add(element);
+        }
+        const arrayNoDupes = Array.from(elements);
+        const quadraticSolution = quadratic(arrayNoDupes);
+        const bruteForceSolution = bruteForce(arrayNoDupes);
+        expect(quadraticSolution).to.equal(bruteForceSolution);
+      }
+    });
+  });
+});
