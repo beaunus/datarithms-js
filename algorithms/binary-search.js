6a943504674386a1d20cd7ef2dbbd7f4d48bc8b3
Beau Dobbin <beau@beaunus.com>
2018-10-24 21:48:22 +0900

    Use non-recursive solution

--- algorithms/binary-search.js
+++ algorithms/binary-search.js
@@ -2,18 +2,20 @@
  * @param {Array} array
  * @param {Number} element
  */
-function indexOf(array, element, start = 0, end = array.length) {
-  if (start >= end) {
-    return -1;
+function indexOf(array, element) {
+  let lo = 0;
+  let hi = array.length - 1;
+  while (lo <= hi) {
+    let mid = Math.floor(lo + (hi - lo) / 2);
+    if (element < array[mid]) {
+      hi = mid - 1;
+    } else if (element > array[mid]) {
+      lo = mid + 1;
+    } else {
+      return mid;
+    }
   }
-  const indexToExamine = Math.floor((start + end) / 2);
-  if (array[indexToExamine] < element) {
-    return indexOf(array, element, indexToExamine + 1, end);
-  }
-  if (array[indexToExamine] > element) {
-    return indexOf(array, element, start, indexToExamine);
-  }
-  return indexToExamine;
+  return -1;
 }
 
 export { indexOf };
