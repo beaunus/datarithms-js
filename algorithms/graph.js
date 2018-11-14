513502ff3722ad9e760084d5a427a57d6380a6ef
Beau Dobbin <beau@beaunus.com>
2018-11-14 21:43:42 +0900

    Finish undirected graph

--- algorithms/graph.js
+++ algorithms/graph.js
@@ -9,24 +9,53 @@ class Graph {
   }
 
   addEdge(thisVertex, thatVertex) {
+    if (thisVertex === thatVertex) {
+      throw new Error("Parallel edges are not allowed.");
+    }
+    this.validateVertex(thisVertex);
+    this.validateVertex(thatVertex);
     this.edges.get(thisVertex).push(thatVertex);
     this.edges.get(thatVertex).push(thisVertex);
     ++this.numEdges;
+  }
+
+  addVertex() {
+    this.edges.set(this.numVertices, []);
+    ++this.numVertices;
   }
 
   degree(vertex) {
     return this.edges.get(vertex).length;
   }
 
+  duplicate() {
+    const copy = new Graph(this.numVertices);
+    copy.numEdges = this.numEdges;
+    for (const [vertex, edges] of this.edges) {
+      if (edges.length) {
+        copy.edges.set(vertex, Array.from(edges));
+      } else {
+        copy.edges.set(vertex, []);
+      }
+    }
+    return copy;
+  }
+
   neighbors(vertex) {
     return this.edges.get(vertex);
+  }
+
+  validateVertex(vertex) {
+    if (vertex < 0 || vertex >= this.numVertices) {
+      throw new RangeError("The given vertex does not exist in the graph.");
+    }
   }
 }
 
 function generateCompleteGraph(numVertices) {
   const graph = new Graph(numVertices);
   for (let thisVertex = 0; thisVertex < numVertices; ++thisVertex) {
-    for (let thatVertex = thisVertex; thatVertex < numVertices; ++thatVertex) {
+    for (let thatVertex = thisVertex + 1; thatVertex < numVertices; ++thatVertex) {
       graph.addEdge(thisVertex, thatVertex);
     }
   }
--- algorithms/graph.spec.js
+++ algorithms/graph.spec.js
@@ -1,12 +1,39 @@
+/* eslint-disable max-len */
+
 import { expect } from "chai";
 
 import { Graph, generateCompleteGraph } from "./graph";
 import { range } from "./itertools";
 
-const MAX_NUM_EDGES = 5;
 const MAX_NUM_VERTICES = 10;
 
-describe("graph", () => {
+describe.only("graph", () => {
+  describe("addEdge", () => {
+    it("should throw an error if either of the vertices don't exist in the graph", () => {
+      for (let numVertices = 0; numVertices < MAX_NUM_VERTICES; ++numVertices) {
+        const graph = new Graph(numVertices);
+        for (let vertex = 0; vertex < numVertices; ++vertex) {
+          expect(() => graph.addEdge(-1, vertex)).to.throw(RangeError);
+        }
+      }
+    });
+    it("should throw an error if the given vertices are the same", () => {
+      for (let numVertices = 0; numVertices < MAX_NUM_VERTICES; ++numVertices) {
+        const graph = new Graph(numVertices);
+        for (let vertex = 0; vertex < numVertices; ++vertex) {
+          expect(() => graph.addEdge(vertex, vertex)).to.throw(Error, /parallel edges/i);
+        }
+      }
+    });
+    it("should not throw an error if the given vertices are not the same", () => {
+      for (let numVertices = 0; numVertices < MAX_NUM_VERTICES; ++numVertices) {
+        const graph = new Graph(numVertices);
+        for (let vertex = 1; vertex < numVertices; ++vertex) {
+          expect(() => graph.addEdge(vertex, vertex - 1)).to.not.throw();
+        }
+      }
+    });
+  });
   describe("degree", () => {
     it("should return 0 for all vertices on a new graph", () => {
       for (let numVertices = 0; numVertices < MAX_NUM_VERTICES; ++numVertices) {
@@ -16,30 +43,40 @@ describe("graph", () => {
         }
       }
     });
-    it("should return 2 for all vertices that have a single self-referencing edge", () => {
-      for (let numVertices = 0; numVertices < MAX_NUM_VERTICES; ++numVertices) {
-        const graph = new Graph(numVertices);
-        for (let vertex = 0; vertex < numVertices; ++vertex) {
-          graph.addEdge(vertex, vertex);
-          expect(graph.degree(vertex)).to.equal(2);
-        }
-      }
-    });
-    it("should return 4 for all vertices that have 2 parallel self-referencing edges", () => {
-      for (let numVertices = 0; numVertices < MAX_NUM_VERTICES; ++numVertices) {
-        const graph = new Graph(numVertices);
-        for (let vertex = 0; vertex < numVertices; ++vertex) {
-          graph.addEdge(vertex, vertex);
-          graph.addEdge(vertex, vertex);
-          expect(graph.degree(vertex)).to.equal(4);
-        }
-      }
-    });
-    it("should return numVertices + 1 for all vertices in a complete graph", () => {
+    it("should return numVertices - 1 for all vertices in a complete graph", () => {
       for (let numVertices = 0; numVertices < MAX_NUM_VERTICES; ++numVertices) {
         const graph = generateCompleteGraph(numVertices);
         for (let vertex = 0; vertex < numVertices; ++vertex) {
-          expect(graph.degree(vertex)).to.equal(numVertices + 1);
+          expect(graph.degree(vertex)).to.equal(numVertices - 1);
+        }
+      }
+    });
+  });
+  describe("duplicate", () => {
+    it("should return a graph that is identical to the original", () => {
+      for (let numVertices = 0; numVertices < MAX_NUM_VERTICES; ++numVertices) {
+        const originalGraph = generateCompleteGraph(numVertices);
+        const copyGraph = originalGraph.duplicate();
+        expect(copyGraph.numVertices).to.equal(originalGraph.numVertices);
+        expect(copyGraph.numEdges).to.equal(originalGraph.numEdges);
+        for (let vertex = 0; vertex < originalGraph.numVertices; ++vertex) {
+          expect(copyGraph.neighbors(vertex)).to.deep.equal(originalGraph.neighbors(vertex));
+        }
+      }
+    });
+    it("should return a deep copy of the original graph", () => {
+      for (let numVertices = 0; numVertices < MAX_NUM_VERTICES; ++numVertices) {
+        const originalGraph = generateCompleteGraph(numVertices);
+        const copyGraph = originalGraph.duplicate();
+        Object.freeze(originalGraph);
+        copyGraph.addVertex();
+        const newVertext = copyGraph.numVertices - 1;
+        expect(copyGraph.numVertices).to.equal(originalGraph.numVertices + 1);
+        for (let vertex = 0; vertex < originalGraph.numVertices; ++vertex) {
+          copyGraph.addEdge(newVertext, vertex);
+          const numAdditionalVertices = vertex + 1;
+          expect(copyGraph.numEdges).to.equal(originalGraph.numEdges + numAdditionalVertices);
+          expect(copyGraph.neighbors(vertex)).to.not.deep.equal(originalGraph.neighbors(vertex));
         }
       }
     });
@@ -53,13 +90,14 @@ describe("graph", () => {
         }
       }
     });
-    it("should return an array containing all vertices in a complete graph", () => {
+    it("should return an array containing all vertices (except the vertex in question) in a complete graph", () => {
       for (let numVertices = 0; numVertices < MAX_NUM_VERTICES; ++numVertices) {
         const graph = generateCompleteGraph(numVertices);
         const allVertices = range(numVertices);
         for (let vertex = 0; vertex < numVertices; ++vertex) {
-          const expected = Array.from(allVertices);
-          expected.push(vertex);
+          const otherVertices = new Set(allVertices);
+          otherVertices.delete(vertex);
+          const expected = Array.from(otherVertices);
           expect(graph.neighbors(vertex).sort()).to.deep.equal(expected.sort());
         }
       }
@@ -74,17 +112,10 @@ describe("graph", () => {
     });
   });
   describe("numEdges", () => {
-    it("should return the correct number of edges when the graph has a single vertex", () => {
-      const graph = new Graph(1);
-      for (let i = 0; i < MAX_NUM_EDGES; ++i) {
-        expect(graph.numEdges).to.equal(i);
-        graph.addEdge(0, 0);
-      }
-    });
     it("should return the correct number of edges in a complete graph", () => {
       for (let numVertices = 1; numVertices < MAX_NUM_VERTICES; ++numVertices) {
         const graph = generateCompleteGraph(numVertices);
-        const expectedNumEdges = (numVertices * (numVertices - 1)) / 2 + numVertices;
+        const expectedNumEdges = (numVertices * (numVertices - 1)) / 2;
         expect(graph.numEdges).to.equal(expectedNumEdges);
       }
     });
