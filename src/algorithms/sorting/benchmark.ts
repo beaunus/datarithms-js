395a40f302fbca3346be269efc9bdb2fcc53e995
Beau Dobbin <beau@beaunus.com>
2019-05-18 15:29:34 +0900

    benchmark sorting different types of arrays

--- src/algorithms/sorting/benchmark.ts
+++ src/algorithms/sorting/benchmark.ts
@@ -1,34 +1,61 @@
 /* tslint:disable:no-unused-expression */
 
 import * as Benchmark from "benchmark";
+import * as chalk from "chalk";
 import * as _ from "lodash";
 
 import { insertionSort } from "./insertion-sort";
 import { quickSortAlgs4 } from "./quick-sort.algs4";
 import { quickSortWikipedia } from "./quick-sort.wikipedia";
 
-Benchmark.op;
-const suite = new Benchmark.Suite();
-
 const NUM_ELEMENTS = 1000;
-const unshuffledArray = _.range(NUM_ELEMENTS);
+const element = Math.random();
+const arrayOfIdenticalNumbers = _.times(NUM_ELEMENTS, () => element);
+const sortedArrayOfIntegers = _.range(NUM_ELEMENTS);
+const sortedArrayOfStrings = _.cloneDeep(sortedArrayOfIntegers).map(x => `${x}`);
 
 const builtIn = array => array.sort();
 const FUNCTIONS_TO_TEST = [builtIn, insertionSort, quickSortAlgs4, quickSortWikipedia];
 
-for (const func of FUNCTIONS_TO_TEST) {
-  suite.add(`${func.name}`, () => {
-    const array = _.shuffle(unshuffledArray);
-    func(array);
-  });
-}
+const typesOfInput = {
+  arrayOfIdenticalNumbers: () => arrayOfIdenticalNumbers,
+  shuffledArrayOfIntegers: () => _.shuffle(sortedArrayOfIntegers),
+  shuffledArrayOfStrings: () => _.shuffle(sortedArrayOfStrings),
+  sortedArrayOfIntegers: () => _.cloneDeep(sortedArrayOfIntegers),
+  sortedArrayOfStrings: () => _.cloneDeep(sortedArrayOfStrings),
+};
 
-suite
-  .on("cycle", event => {
-    console.log(String(event.target));
-  })
-  .on("complete", function() {
-    console.log("Fastest is " + this.filter("fastest").map("name"));
-  })
-  // run async
-  .run({ async: true });
+let hue = 0;
+
+for (const [name, functionToGenerateInput] of Object.entries(typesOfInput)) {
+  const suite = new Benchmark.Suite(name);
+  for (const functionToTest of FUNCTIONS_TO_TEST) {
+    suite.add(`${functionToTest.name}`, () => functionToTest(functionToGenerateInput()), {
+      maxTime: 0.1,
+    });
+  }
+
+  const result = {};
+
+  suite
+    .on("cycle", event => {
+      const [functionName, resultDetails] = String(event.target).split(" x ");
+      result[functionName] = resultDetails;
+    })
+    .on("complete", function(event) {
+      console.log(
+        chalk.default.hsv(hue % 255, 100, 100)("Fastest is " + this.filter("fastest").map("name")),
+      );
+      console.table(result);
+      console.log(
+        chalk.default.hsv(hue % 255, 100, 100)(
+          "--------------------------------------------------------------------------------",
+        ),
+      );
+    })
+    .on("start", function() {
+      console.log(chalk.default.hsv(hue % 255, 100, 100)(this.name));
+    })
+    .run({ async: false });
+  hue += 100;
+}
