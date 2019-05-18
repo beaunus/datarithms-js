602d7be924f2321a7d922a35e16f9ff4936fb300
Beau Dobbin <beau@beaunus.com>
2019-05-18 20:54:55 +0900

    feature: add benchmarks for swap functions

---
+++ src/algorithms/swap/benchmark.ts
@@ -0,0 +1,29 @@
+/* tslint:disable:no-unused-expression */
+
+import * as _ from "lodash";
+
+import { runBenchmarks } from "../../utils/runBenchmarks";
+
+const array = [1, 2];
+
+const tempVariable = () => {
+  const temp = array[0];
+  array[0] = array[1];
+  array[1] = temp;
+};
+
+const destructuring = () => {
+  [array[0], array[1]] = [array[1], array[0]];
+};
+
+const xor = () => {
+  array[0] = array[1] ^ array[0];
+  array[1] = array[1] ^ array[0];
+  array[0] = array[1] ^ array[0];
+}
+
+const FUNCTIONS_TO_TEST = [tempVariable, destructuring, xor];
+
+const INPUT_GENERATORS = [() => array];
+
+runBenchmarks(FUNCTIONS_TO_TEST, INPUT_GENERATORS);
