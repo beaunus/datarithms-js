41037c8dccab43df454431be4e538be39bc700f4
Beau Dobbin <beau@beaunus.com>
2019-05-19 12:31:58 +0900

    get benchmark and eslint working with google style

--- src/utils/runBenchmarks.js
+++ src/utils/runBenchmarks.js
@@ -2,49 +2,42 @@ import * as Benchmark from "benchmark";
 import * as chalk from "chalk";
 import * as _ from "lodash";
 
+/**
+ * @param {Array<Function>} functionsToTest
+ * @param {Array<Function>} inputGenerators
+ */
 function runBenchmarks(functionsToTest, inputGenerators) {
   let hue = _.random(0, 256);
   for (const inputGenerator of inputGenerators) {
     const suite = new Benchmark.Suite(inputGenerator.name);
     for (const functionToTest of functionsToTest) {
-      suite.add(`${functionToTest.name}`, () => functionToTest(inputGenerator()), {
-        maxTime: 0.1,
-      });
+      const { name } = functionToTest;
+      const fn = () => functionToTest(inputGenerator());
+      const options = { maxTime: 0.1 };
+      suite.add(name, fn, options);
     }
     const color = chalk.default.hsl(hue % 255, 100, 80);
     const result = {};
     suite
       .on("cycle", event => {
-        const [functionName, resultDetails] = String(event.target).split(" x ");
-        result[functionName] = resultDetails;
+        result[event.target.name] = event.target.hz;
       })
-      .on("complete", () => {
-        const resultEntries = Object.entries(result).map(([name, description]) => ({
+      .on("complete", event => {
+        const resultEntries = Object.entries(result).map(([name, hz]) => ({
           name,
-          opsPerSecond: getOpsPerSecond(description),
+          hz: Math.floor(hz)
         }));
-        resultEntries.sort((a, b) => b.opsPerSecond - a.opsPerSecond);
-        const sortedResults = {};
-        for (const { name, opsPerSecond } of resultEntries) {
-          sortedResults[name] = opsPerSecond;
-        }
-        console.table(sortedResults);
-        if (this.name) {
-          console.log(color("--------------------"));
-        }
+        resultEntries.sort((a, b) => b.hz - a.hz);
+        console.table(resultEntries);
       })
-      .on("start", function() {
-        if (this.name) {
-          console.log(color(this.name));
-        }
+      .on("start", event => {
+        console.log();
+        if (event.currentTarget.name)
+          console.log(color(event.currentTarget.name));
       })
       .run({ async: false });
     hue += 100;
   }
-}
-
-function getOpsPerSecond(description: {}): number {
-  return Number.parseFloat(`${description}`.split(" ops/sec")[0].replace(/,/g, ""));
 }
 
 export { runBenchmarks };
