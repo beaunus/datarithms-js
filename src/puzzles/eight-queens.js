19d1a094ce9149a51d7fbe589a27081fd0a34ee7
Beau Dobbin <beau@beaunus.com>
2019-07-25 22:25:30 +0900

    feature: write tests and code for eight-queens backtracking

--- src/puzzles/eight-queens.js
+++ src/puzzles/eight-queens.js
@@ -27,3 +27,48 @@ function isSafe(config) {
   }
   return true;
 }
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+export function countSolutionsBacktracking(n) {
+  return countSolutionsBacktrackingHelper({
+    currentRow: 0,
+    placements: [],
+    remainingCols: range(0, n),
+  });
+}
+
+/**
+ * @param {object} param
+ * @param {Array<number>} param.remainingCols
+ * @param {number} param.currentRow
+ * @param {Array<number>} param.placements
+ * @return {number}
+ */
+function countSolutionsBacktrackingHelper({
+  remainingCols,
+  currentRow,
+  placements,
+}) {
+  if (!remainingCols.length) return 1;
+  let result = 0;
+  for (const position of remainingCols) {
+    let isPositionAllowed = true;
+    for (let j = currentRow - 1; j >= 0; --j) {
+      const diff = Math.abs(position - placements[j]);
+      if (diff === currentRow - j) {
+        isPositionAllowed = false;
+        break;
+      }
+    }
+    if (isPositionAllowed)
+      result += countSolutionsBacktrackingHelper({
+        currentRow: currentRow + 1,
+        placements: [...placements, position],
+        remainingCols: remainingCols.filter(x => x !== position),
+      });
+  }
+  return result;
+}
--- src/puzzles/eight-queens.spec.js
+++ src/puzzles/eight-queens.spec.js
@@ -1,12 +1,53 @@
-import { countSolutionsBruteForce } from "./eight-queens";
+import {
+  countSolutionsBacktracking,
+  countSolutionsBruteForce,
+} from "./eight-queens";
 import { expect } from "chai";
 
-describe("eight-queens", () => {
-  const solutions = [1, 1, 0, 0, 2, 10, 4, 40, 92, 352];
+describe.only("eight-queens", () => {
+  const solutions = [
+    1,
+    1,
+    0,
+    0,
+    2,
+    10,
+    4,
+    40,
+    92,
+    352,
+    724,
+    2680,
+    14200,
+    73712,
+    365596,
+    2279184,
+    14772512,
+    95815104,
+    666090624,
+    4968057848,
+    39029188884,
+    314666222712,
+    2691008701644,
+    24233937684440,
+    227514171973736,
+    2207893435808352,
+    22317699616364044,
+    234907967154122528,
+  ];
   describe("countSolutionsBruteForce", () => {
     it("should return the correct answer", () => {
-      for (let i = 1; i < solutions.length; ++i) {
+      for (let i = 0; i < 9; ++i) {
         const actual = countSolutionsBruteForce(i);
+        expect(actual).to.equal(solutions[i]);
+      }
+    });
+  });
+
+  describe("countSolutionsBacktracking", () => {
+    it("should return the correct answer", () => {
+      for (let i = 0; i < 11; ++i) {
+        const actual = countSolutionsBacktracking(i);
         expect(actual).to.equal(solutions[i]);
       }
     });
